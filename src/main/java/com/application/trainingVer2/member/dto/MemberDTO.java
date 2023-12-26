b500370e6694824a69ec9ec8de85d1493127aa5d
DESKTOP-HHECRK8\19_web_lsh <sh-119@naver.com>
2023-12-26 20:29:38 +0900

    lombok 설치 및 사용

--- src/main/java/com/application/trainingVer2/member/dto/MemberDTO.java
+++ src/main/java/com/application/trainingVer2/member/dto/MemberDTO.java
@@ -4,6 +4,33 @@ import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+/*
+
+	# 롬복(lombok)
+	
+	- 롬복은 반복적이고 번거로운 코드를 줄이고, 개발자가 간편하게 자바 빈(Java Bean) 클래스를 작성할 수 있도록 도와주는 라이브러리입니다.
+	- 롬복은 주로 어노테이션(Annotation)을 활용하여 코드를 자동으로 생성해주는 기능을 제공합니다. 
+	- @Data
+		@Data 어노테이션 사용시 
+		Getter와 , Setter , toString() , 기본 생성자 , equals() , hashCode()가 모두 추가되어
+		코드의 가독성을 높이고 유지보수를 용이하게 만들 수 있습니다.
+	
+	- 구현방법
+	
+		 1) build.gradle파일에 lombok 의존성 추가(프로젝트 생성시 선택가능)
+		    implementation 'org.projectlombok:lombok'
+		 
+		 @) 구현이 안될 경우
+			Help menu > Install new software > (Add) https://projectlombok.org/p2를 설치하면 롬복을 구현할 수 있습니다.
+		
+*/
+@Data
 @Component
 public class MemberDTO {
 
@@ -26,129 +53,5 @@ public class MemberDTO {
 	private String activeYn;
 	private Date inactiveAt;
 	private Date joinAt;
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-	public String getPasswd() {
-		return passwd;
-	}
-	public void setPasswd(String passwd) {
-		this.passwd = passwd;
-	}
-	public String getProfileOriginalName() {
-		return profileOriginalName;
-	}
-	public void setProfileOriginalName(String profileOriginalName) {
-		this.profileOriginalName = profileOriginalName;
-	}
-	public String getProfileUUID() {
-		return profileUUID;
-	}
-	public void setProfileUUID(String profileUUID) {
-		this.profileUUID = profileUUID;
-	}
-	public String getMemberNm() {
-		return memberNm;
-	}
-	public void setMemberNm(String memberNm) {
-		this.memberNm = memberNm;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public String getBirthAt() {
-		return birthAt;
-	}
-	public void setBirthAt(String birthAt) {
-		this.birthAt = birthAt;
-	}
-	public String getHp() {
-		return hp;
-	}
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-	public String getSmsstsYn() {
-		return smsstsYn;
-	}
-	public void setSmsstsYn(String smsstsYn) {
-		this.smsstsYn = smsstsYn;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getEmailstsYn() {
-		return emailstsYn;
-	}
-	public void setEmailstsYn(String emailstsYn) {
-		this.emailstsYn = emailstsYn;
-	}
-	public String getZipcode() {
-		return zipcode;
-	}
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
-	public String getRoadAddress() {
-		return roadAddress;
-	}
-	public void setRoadAddress(String roadAddress) {
-		this.roadAddress = roadAddress;
-	}
-	public String getJibunAddress() {
-		return jibunAddress;
-	}
-	public void setJibunAddress(String jibunAddress) {
-		this.jibunAddress = jibunAddress;
-	}
-	public String getNamujiAddress() {
-		return namujiAddress;
-	}
-	public void setNamujiAddress(String namujiAddress) {
-		this.namujiAddress = namujiAddress;
-	}
-	public String getEtc() {
-		return etc;
-	}
-	public void setEtc(String etc) {
-		this.etc = etc;
-	}
-	public String getActiveYn() {
-		return activeYn;
-	}
-	public void setActiveYn(String activeYn) {
-		this.activeYn = activeYn;
-	}
-	public Date getInactiveAt() {
-		return inactiveAt;
-	}
-	public void setInactiveAt(Date inactiveAt) {
-		this.inactiveAt = inactiveAt;
-	}
-	public Date getJoinAt() {
-		return joinAt;
-	}
-	public void setJoinAt(Date joinAt) {
-		this.joinAt = joinAt;
-	}
-	
-	@Override
-	public String toString() {
-		return "MemberDTO [memberId=" + memberId + ", passwd=" + passwd + ", profileOriginalName=" + profileOriginalName
-				+ ", profileUUID=" + profileUUID + ", memberNm=" + memberNm + ", sex=" + sex + ", birthAt=" + birthAt
-				+ ", hp=" + hp + ", smsstsYn=" + smsstsYn + ", email=" + email + ", emailstsYn=" + emailstsYn
-				+ ", zipcode=" + zipcode + ", roadAddress=" + roadAddress + ", jibunAddress=" + jibunAddress
-				+ ", namujiAddress=" + namujiAddress + ", etc=" + etc + ", activeYn=" + activeYn + ", inactiveAt="
-				+ inactiveAt + ", joinAt=" + joinAt + "]";
-	}
 	
 }
